a98213b87c2ae706531cc89eb617538aab28c67c
Ryan Thames <rthames62@gmail.com>
2016-08-25 11:45:02 -0500

    fixed chunky monkey

--- basic/chunky-monkey.js
+++ basic/chunky-monkey.js
@@ -1,28 +1,13 @@
 //Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
 function chunkArrayInGroups(arr, size) {
+	var newArr = [];
+	for(var i = 0; i < arr.length; i += size) {
+		newArr.push(arr.slice(i, i + size));
+	}
 
-  var temp = [];
-  var result = [];
-
-
-  for (var i = 0; i < arr.length; i++) {
-    if (i % size !== size - 1) {
-      temp.push(arr[i]);
-    }
-    else {
-      temp.push(arr[i]);
-      result.push(temp);
-      temp = [];
-    }
-  }
-
-  if (temp.length !== 0) {
-    result.push(temp);
-  }
-  return result;
+	return newArr;
 }
 
 
-
-chunkArrayInGroups(["a", "b", "c", "d", "e"], 3);
+chunkArrayInGroups(['1','2','3','4','5','6','7','8','9','0'], 4);
